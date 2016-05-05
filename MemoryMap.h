7227b06bb0ddad09874533e11aeea2b310087b3e
MahmoudRizk <mahmoudahmedrizk@gmail.com>
2016-05-05 15:30:31 +0200

    adding MEMORY  structure and MAIN program

--- MemoryMap.h
+++ MemoryMap.h
@@ -14,6 +14,7 @@ class Node
     Node(std::string n, int BAddr, int s);
     Node(int BAddr, int s);
     Node(string n,int s);
+    Node();
     std::string get_name();
     int get_base_address();
     int get_size();
@@ -40,6 +41,7 @@ public:
   int  search_first_fit(int s);
   int  search_worst_fit(int s);
   int  search_best_fit(int s);
+  friend class Memory;
 
 };
 
@@ -53,11 +55,20 @@ class ProcessesTable
   bool search_base_address(int BAddr);
   Node search_base_address_2(int BAddr);
   int  get_table_size();
+  friend class Memory;
 
 
 
 };
-class Memory{};  //Under construction :)
+class Memory
+{
+  Node *memory=new Node[10];
+  public:
+ void update_memory(HoleTable h1, ProcessesTable t1);
+ void print_memory_by_name();
+ void print_memory_in_detail();
+
+};  //Under construction :)
 
 int table_sync_add_process(HoleTable &h1, ProcessesTable &t1, Node &n);
 void table_sync_delete_process(HoleTable &h1, ProcessesTable &t1, Node n);
@@ -66,5 +77,5 @@ void initial_Processes_table_filling(HoleTable h1, ProcessesTable &t1, int memor
 void holes_initialize(HoleTable &h1);
 void input_processes(list<Node> &list_processes);
 void process_processes(list<Node> &list_processes, HoleTable &h1, ProcessesTable &t1);
-void tables_handler(list<Node> &list_processes, HoleTable &h1, ProcessesTable &t1);
+void tables_handler(list<Node> &list_processes, HoleTable &h1, ProcessesTable &t1, Memory &memory);
 #endif // MEMORYMAP_INCLUDED
